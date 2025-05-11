a8add9d3c8e0a472f8c89bf3c98b64c9fdda9ce1
bamboo-nova <suzuk1nov11@gmail.com>
2025-05-11 17:12:06 +0900

    :rocket: first commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # rust-airtags
+
+## Usage
+
+- .envファイルを以下に必要な項目を入れてrust-airtags直下に作成する
+
+```
+APPLE_ID=hogehoge
+APPLE_APP_PASSWORD=fugafuga
+```
+
+- apple-private-apisのリポジトリをrust-airtagsと同じ階層に置く
+
+- apple-private-apis/omnisette/aos_kit.rsをrust-airtags/fixed_codes/aos_kit.rsに置き換える。
+
+- cargo runを実施する
+  - ただし、rustc 1.86.0以上が必要なので、実行したい場合はバージョンを上げる必要があるので注意
---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use icloud_auth::AppleAccount;
+use omnisette::AnisetteConfiguration;
+use anyhow::Context;
+use dotenvy::dotenv;
+use std::env;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenv().ok();
+    let apple_id = env::var("APPLE_ID")
+        .context("環境変数 APPLE_ID が設定されていません")?;
+    let apple_pw = env::var("APPLE_APP_PASSWORD")
+        .context("環境変数 APPLE_APP_PASSWORD が設定されていません")?;
+
+
+    // 1. 端末ローカルの Anisette 情報を自動生成（macOS / iOS 互換）
+    let anisette_cfg = AnisetteConfiguration::new();
+    let mut acc = AppleAccount::new(anisette_cfg).await?;
+
+    // 2. メール & パスワードで SRP ログイン（A2k → M1/M2 まで）
+    match acc.login_email_pass(&apple_id, &apple_pw).await? {
+        icloud_auth::LoginState::LoggedIn => {
+            println!("ログイン完了。GsIdmsToken = {:?}", acc.get_pet());
+        }
+        next_state => {
+            println!("追加認証が必要: {:?}", next_state);
+        }
+    }
+
+    Ok(())
+}
